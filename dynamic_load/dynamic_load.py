9999a8a3375d33980c79ac30ed7e6c68712fee28
Benjamin Jin <benjamin.jin@semifive.com>
2021-10-07 17:46:26 +0900

    Update for packaging

--- dynamic_load/dynamic_load.py
+++ dynamic_load/dynamic_load.py
@@ -1,32 +1,12 @@
 import importlib
 import sys
-class Recurse(Exception):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-
-def recurse(*args, **kwargs):
-    raise Recurse(*args, **kwargs)
-
-def tail_recursive(f):
-    def decorated(*args, **kwargs):
-        while True:
-            try:
-                return f(*args, **kwargs)
-            except Recurse as r:
-                args = r.args
-                kwargs = r.kwargs
-                continue
-    return decorated
-
 def dynamic_load(string : str) :
-  @tail_recursive
   def load_module(target : str, remained : list = list()) :
     try:
       module = importlib.import_module(target)
       return (module, remained)
     except ModuleNotFoundError:
-      recurse(target=".".join(target.split(".")[:-1]), remained=[target.split(".")[-1]] + remained)
+      return load_module(target=".".join(target.split(".")[:-1]), remained=[target.split(".")[-1]] + remained)
     except ValueError:
       sys.exit("Cannot resolve any module from {}".format(string))
   module, last = load_module(string)
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(name='dymanic_load',
       version='0.0.1',
-      url='https://github.com/benjamin-jin/python-dynamic-exeuction', # 프로젝트 주소
-      author='Benjamin Jin', # 작성자
-      author_email='jinrudals135@naver.com', # 작성자 이메일
-      description='Load module, function from string', # 간단한 설명
-      packages=find_packages(exclude=['']),  # 기본 프로젝트 폴더 외에 추가로 입력할 폴더
-      long_description=open('README.md').read(), # 프로젝트 설명, 보통 README.md로 관리
-      install_requires=[''], # 설치시 설치할 라이브러리
+      url='https://github.com/benjamin-jin/python-dynamic-exeuction',
+      author='Benjamin Jin',
+      author_email='jinrudals135@naver.com',
+      description='Load module, function from string',
+      packages=find_packages(exclude=['']),
+      long_description=open('README.md').read(),
+      install_requires=[''],
 )
