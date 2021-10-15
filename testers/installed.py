54df845e2d580343f58929853d0eacfd2ab726c1
Benjamin Jin <benjamin.jin@semifive.com>
2021-10-15 10:36:31 +0900

    Update installed test scenario

--- testers/installed.py
+++ testers/installed.py
@@ -1,9 +1,21 @@
+#!/bin/env python3
 from dynamic_load import *
 import inspect
-# @tail_call_optimized
+@tail_call_optimized
 def factorial(n, acc=1):
   "calculate a factorial"
   if n == 0:
     return acc
   return factorial(n-1, n*acc)
-print(inspect.getsource(factorial))
+
+factorial(4)
+
+try:
+  @tail_call_optimized
+  def fibo(n : int) -> int:
+    if (n < 2):
+      return 1
+    else:
+      return fibo(n-1) + fibo(n+2)
+except NotTailRecursive:
+  print("Expected Exception for non-tail-recursive function")
